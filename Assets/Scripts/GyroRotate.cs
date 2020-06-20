bc0199b89fde24c5a91a206f78cbdc76d48bfb93
Fernanda <acquaentertainment@gmail.com>
2020-06-20 15:51:37 -0700

    probar juego en iphone

---
+++ Assets/Scripts/GyroRotate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+public class GyroRotate : MonoBehaviour
+{
+
+    private Gyroscope gyro;
+
+    void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+        }
+        else
+        {
+            Debug.Log("Phone doesen't support");
+        }
+    }
+
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Gyroscope attitude : " + gyro.attitude);
+    }
+}
