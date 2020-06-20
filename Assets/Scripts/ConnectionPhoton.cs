7145fba759e5a8b88051fa70fc67651307b982a8
Fernanda <acquaentertainment@gmail.com>
2020-06-20 11:37:47 -0700

    cambios photon

--- Assets/Scripts/ConnectionPhoton.cs
+++ Assets/Scripts/ConnectionPhoton.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class ConnectionPhoton : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
