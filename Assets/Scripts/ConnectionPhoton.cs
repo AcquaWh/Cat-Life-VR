53cf5d5026db5be4dac9139fa7deeddf051452b0
Fernanda <acquaentertainment@gmail.com>
2020-06-20 14:20:23 -0700

    conexion

--- Assets/Scripts/ConnectionPhoton.cs
+++ Assets/Scripts/ConnectionPhoton.cs
@@ -1,19 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 public class ConnectionPhoton : MonoBehaviourPunCallbacks
 {
+    /*
     [SerializeField]
     QuickStartLobbyController quickStartLobby;
+    */
+    public Button ConnectButton;
+    public Button JoinRandomButton;
+    public Text Log;
+    public byte maxPlayersPerRoom = 4;
 
-    void Start()
+    public void Connect()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        if (!PhotonNetwork.IsConnected)
+        {
+            Log.text = "\nConnected to Server";
+            if (PhotonNetwork.ConnectUsingSettings())
+            {
+                Log.text += "\nConnected to Server";
+            } else
+            {
+                Log.text += "\nFailing Connecting to Server";
+            }
+        }
     }
+
     public override void OnConnectedToMaster()
     {
-        Debug.Log($"Connected to {PhotonNetwork.CloudRegion} server");
-        quickStartLobby.QuickStart();
+        ConnectButton.interactable = false;
+        JoinRandomButton.interactable = true;
+    }
+    public void JoinRandom()
+    {
+        if (PhotonNetwork.JoinRandomRoom())
+        {
+            Log.text += "\nJoinned Room";
+        } else
+        {
+            Log.text += "\nFail Joining";
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Log.text += "\nNo Rooms to Join, creating one...";
+        if(PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions() { MaxPlayers = maxPlayersPerRoom }))
+        {
+            Log.text += "\nRoom Created";
+        } else
+        {
+            Log.text += "\nFail Creating Room";
+        }
     }
 }
