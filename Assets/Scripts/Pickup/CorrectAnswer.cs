a6cc3755a694ae94229141e3d22e1d8869cdf7e2
Fernanda <acquaentertainment@gmail.com>
2020-07-15 22:14:13 -0700

    prefinal level

---
+++ Assets/Scripts/Pickup/CorrectAnswer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectAnswer : MonoBehaviour
+{
+    public void Correct()
+    {
+        ScoreScript.scoreValue += 50;
+    }
+}
---
+++ Assets/Scripts/Pickup/WrongAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrongAnswer : MonoBehaviour
+{
+    public AudioClip wrong;
+    public AudioSource audioSource;
+    public void Wrong()
+    {
+        audioSource.PlayOneShot(wrong, 0.7F); 
+    }
+}
