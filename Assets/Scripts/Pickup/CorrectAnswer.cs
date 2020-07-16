21e820c2fff5cf27db225fccf5a00b75e412af38
Fernanda <acquaentertainment@gmail.com>
2020-07-15 22:27:33 -0700

    ultimos test

--- Assets/Scripts/Pickup/CorrectAnswer.cs
+++ Assets/Scripts/Pickup/CorrectAnswer.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CorrectAnswer : MonoBehaviour
 {
+    public AudioClip correct;
+    public AudioSource audioSource;
     public void Correct()
     {
         ScoreScript.scoreValue += 50;
+        audioSource.PlayOneShot(correct, 0.7F);
     }
 }
