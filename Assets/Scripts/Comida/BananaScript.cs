b151b72c814bd03101ed96bafe95d5a0dc37c6ac
Fernanda <acquaentertainment@gmail.com>
2020-07-15 21:54:08 -0700

    activar paneles

---
+++ Assets/Scripts/Comida/BananaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaScript : MonoBehaviour
+{
+    public GameObject Panel;
+    public AudioClip meow;
+    public AudioSource audioSource;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            if(Panel != null)
+            {
+                bool isActive = Panel.activeSelf;
+                Panel.SetActive(true);
+                audioSource.PlayOneShot(meow, 0.7F);
+                ScoreScript.scoreValue += 10;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
